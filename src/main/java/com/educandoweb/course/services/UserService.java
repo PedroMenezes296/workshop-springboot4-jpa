e68af5569e8a0a48855925ce3c32d3763f7efc2e
PedroMenezes296 <barananomenezespedro@gmail.com>
2026-01-27 08:07:05 -0300

    Component registration implementation

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -1,9 +1,12 @@
 package com.educandoweb.course.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
@@ -22,5 +25,11 @@ public class UserService {
 	public List<User> findAll(){
 		return repository.findAll();
 	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
 	
 }
